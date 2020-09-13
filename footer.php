bda8c734d82e74319c8ca9bc4e78c5529a186739
Brijesh Dhanani <46484569+BRdhanani@users.noreply.github.com>
2020-09-13 14:26:59 +0530

    aqwind setup

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Aqwind
+ */
+
+?>
+		<footer>
+			Footer
+		</footer>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Header file for the Aqwind WordPress theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Aqwind
+ */
+?>
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+	<head>
+
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+
+		<?php wp_head(); ?>
+
+	</head>
+
+	<body <?php body_class(); ?>>
+		<header>
+			Header
+		</header>
